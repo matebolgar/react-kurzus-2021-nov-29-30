588885c595c324fc33ea7ebf826cd08f86401f85
kodbazis <mate@kodbazis.hu>
2021-11-30 08:22:47 +0100

    Added project

---
+++ 11-posts-comments/src/App.jsx
@@ -0,0 +1,5 @@
+// Könyvtárak telepítése: npm i
+// Babel indítása: npm run dev
+
+// Kommentek: https://jsonplaceholder.typicode.com/comments
+// Posztok: https://jsonplaceholder.typicode.com/posts

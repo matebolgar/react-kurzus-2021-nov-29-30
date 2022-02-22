d04dd9dff94e2f0e1df62f46439b5f7695733c33
kodbazis <mate@kodbazis.hu>
2022-02-22 13:06:12 +0100

    Added dynamic lists

--- 14-recept-app/app/src/RecipeCreate.js
+++ 14-recept-app/app/src/RecipeCreate.js
@@ -1,4 +1,4 @@
-import { faPlus, faTrash } from "@fortawesome/free-solid-svg-icons";
+import { faCheckCircle, faPlus, faTrash } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useState } from "react";
 
@@ -14,6 +14,13 @@ export function RecipeCreate() {
     }
   ]);
 
+  const [steps, setSteps] = useState([
+    {
+      timer: "",
+      content: "",
+    }
+  ]);
+
 
   return (
     <div className="container">
@@ -22,7 +29,9 @@ export function RecipeCreate() {
         <hr />
         <form onSubmit={(e) => {
           e.preventDefault();
-          console.log(e.target.elements.name.value);
+          console.log(name);
+          console.log(ingredients);
+          console.log(steps);
         }}>
           <div className="form-group row pb-3 border-bottom">
             <label className="col-sm-2 col-form-label">Név:</label>
@@ -74,7 +83,7 @@ export function RecipeCreate() {
                           return next;
                         })
                       }}
-                     
+
                     />
                   </div>
                   <div className="col-md-2">
@@ -138,112 +147,77 @@ export function RecipeCreate() {
           </div>
           <div className="form-group row pb-3 border-bottom">
             <label className="col-sm-2 col-form-label">Lépések:</label>
-            <div className="col-sm-10 offset-2">
-              <div className="row mb-3 border-bottom">
-                <div className="col-md-6">
-                  <input
-                    type="text"
-                    name="step-content-0"
-                    className="form-control d-inline-block mr-2 mb-2"
-                    placeholder="1. lépés"
-                    required=""
-                  />
-                </div>
-                <div className="col-md-2">
-                  <input
-                    type="number"
-                    name="step-timer-0"
-                    className="form-control d-inline-block mr-2 mb-2"
-                    placeholder="Idő"
-                    min="1"
-                    required=""
-                  />
-                </div>
-                <div className="col-md-2">
-                  <div className="w-100">
-                    <button className="btn btn-danger mb-3" type="button">
-                      <svg
-                        aria-hidden="true"
-                        focusable="false"
-                        data-prefix="fas"
-                        data-icon="trash"
-                        className="svg-inline--fa fa-trash fa-w-14"
-                        role="img"
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 448 512"
-                      >
-                        <path
-                          fill="currentColor"
-                          d="M432 32H312l-9.4-18.7A24 24 0 0 0 281.1 0H166.8a23.72 23.72 0 0 0-21.4 13.3L136 32H16A16 16 0 0 0 0 48v32a16 16 0 0 0 16 16h416a16 16 0 0 0 16-16V48a16 16 0 0 0-16-16zM53.2 467a48 48 0 0 0 47.9 45h245.8a48 48 0 0 0 47.9-45L416 128H32z"
-                        ></path>
-                      </svg>
-                    </button>
+
+            {steps.map((step, i) => (
+              <div key={i} className="col-sm-10 offset-2">
+                <div className="row mb-3 border-bottom">
+                  <div className="col-md-6">
+                    <input
+                      type="text"
+                      name="step-content-0"
+                      className="form-control d-inline-block mr-2 mb-2"
+                      placeholder="1. lépés"
+                      value={step.content}
+                      onChange={(e) => {
+                        e.persist();
+                        setSteps((prev) => {
+                          const next = [...prev];
+                          next[i].content = e.target.value;
+                          return next;
+                        })
+                      }}
+                      required=""
+                    />
+                  </div>
+                  <div className="col-md-2">
+                    <input
+                      type="number"
+                      name="step-timer-0"
+                      className="form-control d-inline-block mr-2 mb-2"
+                      placeholder="Idő"
+                      min="1"
+                      value={step.timer}
+                      onChange={(e) => {
+                        e.persist();
+                        setSteps((prev) => {
+                          const next = [...prev];
+                          next[i].timer = e.target.value;
+                          return next;
+                        })
+                      }}
+                      required=""
+                    />
+                  </div>
+                  <div className="col-md-2">
+                    <div className="w-100">
+                      <button className="btn btn-danger mb-3" type="button"
+                        onClick={() => {
+                          setSteps((prev) => {
+                            const next = prev.slice();
+                            next.splice(i, 1)
+                            return next;
+                          })
+                        }}>
+                        <FontAwesomeIcon icon={faTrash} />
+                      </button>
+                    </div>
                   </div>
                 </div>
-              </div>
-            </div>
-            <div className="col-sm-10 offset-2">
-              <div className="row mb-3">
-                <div className="col-md-6">
-                  <input
-                    type="text"
-                    name="step-content-1"
-                    className="form-control d-inline-block mr-2 mb-2"
-                    placeholder="2. lépés"
-                    required=""
-                  />
-                </div>
-                <div className="col-md-2">
-                  <input
-                    type="number"
-                    name="step-timer-1"
-                    className="form-control d-inline-block mr-2 mb-2"
-                    placeholder="Idő"
-                    min="1"
-                    required=""
-                  />
-                </div>
-                <div className="col-md-2">
-                  <div className="w-100">
-                    <button className="btn btn-danger mb-3" type="button">
-                      <svg
-                        aria-hidden="true"
-                        focusable="false"
-                        data-prefix="fas"
-                        data-icon="trash"
-                        className="svg-inline--fa fa-trash fa-w-14"
-                        role="img"
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 448 512"
-                      >
-                        <path
-                          fill="currentColor"
-                          d="M432 32H312l-9.4-18.7A24 24 0 0 0 281.1 0H166.8a23.72 23.72 0 0 0-21.4 13.3L136 32H16A16 16 0 0 0 0 48v32a16 16 0 0 0 16 16h416a16 16 0 0 0 16-16V48a16 16 0 0 0-16-16zM53.2 467a48 48 0 0 0 47.9 45h245.8a48 48 0 0 0 47.9-45L416 128H32z"
-                        ></path>
-                      </svg>
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </div>
+              </div>))}
+
+
             <div className="row w-100">
               <div className="col-md-3 offset-md-9">
-                <button type="button" className="btn btn-success btn-sm float-right">
-                  <svg
-                    aria-hidden="true"
-                    focusable="false"
-                    data-prefix="fas"
-                    data-icon="plus"
-                    className="svg-inline--fa fa-plus fa-w-14"
-                    role="img"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 448 512"
-                  >
-                    <path
-                      fill="currentColor"
-                      d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"
-                    ></path>
-                  </svg>
+                <button type="button" className="btn btn-success btn-sm float-right" onClick={() => {
+                  setSteps(prev => {
+                    return [
+                      ...prev, {
+                        timer: "",
+                        content: "",
+                      }]
+                  })
+                }}>
+                  <FontAwesomeIcon icon={faPlus} />
                 </button>
               </div>
             </div>
@@ -258,21 +232,7 @@ export function RecipeCreate() {
           </div>
           <button type="submit" className="btn btn-success btn-sm">
             Küldés
-            <svg
-              aria-hidden="true"
-              focusable="false"
-              data-prefix="fas"
-              data-icon="check-circle"
-              className="svg-inline--fa fa-check-circle fa-w-16"
-              role="img"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 512 512"
-            >
-              <path
-                fill="currentColor"
-                d="M504 256c0 136.967-111.033 248-248 248S8 392.967 8 256 119.033 8 256 8s248 111.033 248 248zM227.314 387.314l184-184c6.248-6.248 6.248-16.379 0-22.627l-22.627-22.627c-6.248-6.249-16.379-6.249-22.628 0L216 308.118l-70.059-70.059c-6.248-6.248-16.379-6.248-22.628 0l-22.627 22.627c-6.248 6.248-6.248 16.379 0 22.627l104 104c6.249 6.249 16.379 6.249 22.628.001z"
-              ></path>
-            </svg>
+            <FontAwesomeIcon icon={faCheckCircle} />
           </button>
         </form>
       </div>

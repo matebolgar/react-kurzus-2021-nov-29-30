2889eee81c06b32b7746ce2c0ff588c3543e8874
kodbazis <mate@kodbazis.hu>
2022-02-23 17:08:34 +0100

    Comments added

--- 17.2-jwt/src/AuthService.ts
+++ 17.2-jwt/src/AuthService.ts
@@ -8,7 +8,8 @@ let accessToken = "";
 //   console.log(accessToken);
 // }, 1000);
 
-export function login(email: any, password: any) {
+export function login(email: string, password: string) {
+  // fetch("...", {credentials: "include"})
   return axios
     .post("https://kodbazis.hu/api/login-user", { email, password }, { withCredentials: true })
     .then((res) => {
@@ -25,11 +26,15 @@ export function logout() {
 
 export const fetchHitelesitessel = axios.create();
 
+// Kimenő kérést módosító interceptor
 fetchHitelesitessel.interceptors.request.use(
   (config) => {
     if (!accessToken) {
       return config;
     }
+
+    console.log(config);
+    
 
     return {
       ...config,
@@ -42,26 +47,34 @@ fetchHitelesitessel.interceptors.request.use(
   (error) => Promise.reject(error)
 );
 
+// Választ módosító interceptor
 fetchHitelesitessel.interceptors.response.use(
   (response) => response,
   (error) => {
+    // Valami hiba van
     if (error.response.status !== 403) {
+      // Nem authentikációs jellegű hiba van
       return Promise.reject(error);
     }
 
+    // Lemásoljuk a request adatokat
     const originalRequest = error.config;
 
     if (originalRequest.isRetry) {
+      // Már egyszer meg lett kísérelve a token megújítása 
       return Promise.reject(error);
     }
 
+    // Megjelöljük a configban, hogy kísérlet történik a megújításra
     originalRequest.isRetry = true;
 
+    // Kimegy az access tokent megújító kérés
     return axios
       .get("https://kodbazis.hu/api/get-new-access-token", {
         withCredentials: true,
       })
       .then((res) => {
+        // Sikerült megújítani a tokent -> eltároljuk a memóriába
         accessToken = res.data.accessToken;
       })
       .then(() => fetchHitelesitessel(originalRequest));
